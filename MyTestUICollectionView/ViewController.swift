83aebeb80b50a82dd89972801c45aaf84c08eff0
Sergey <dir@syakovlev.ru>
2020-03-02 17:17:06 +0500

    add set layout, constrant
    test

--- MyTestUICollectionView/ViewController.swift
+++ MyTestUICollectionView/ViewController.swift
@@ -9,31 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-    //UICollectionViewDelegateFlowLayout
+    
     let registerCell = "RegCollectionCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .green
-        let layout = UICollectionViewFlowLayout()
+        view.backgroundColor = .white
         
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: view.frame.width, height: 100)
         //layout.scrollDirection = .horizontal
         let myCollectionView = UICollectionView.init(frame: view.bounds, collectionViewLayout: layout)
         
         myCollectionView.delegate = self
         myCollectionView.dataSource = self
+        myCollectionView.translatesAutoresizingMaskIntoConstraints = false
         myCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: registerCell)
         view.addSubview(myCollectionView)
+        
+        myCollectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        myCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        myCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        myCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
         // Do any additional setup after loading the view.
     }
     
+    func setupCollectionConstraints() {
+        
+    }
+    
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        5
+        1
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 1
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "registerCell", for: indexPath)
@@ -41,7 +53,5 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         return cell
     }
     
-    
-// test 
 }
 

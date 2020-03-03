0e3daf2bdaf2b837ef8a868ce118b5369f9d137f
Sergey <dir@syakovlev.ru>
2020-03-03 15:39:58 +0500

    add text

--- MyTestUICollectionView/ViewController.swift
+++ MyTestUICollectionView/ViewController.swift
@@ -15,17 +15,17 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        view.backgroundColor = .blue
         
         let layout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.itemSize = CGSize(width: 300, height: 100) //view.frame.width
-        //layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+        layout.itemSize = CGSize(width: 150, height: 50) //view.frame.width
+        layout.scrollDirection = .horizontal
         let myCollectionView = UICollectionView.init(frame: view.bounds, collectionViewLayout: layout)
         
         myCollectionView.delegate = self
         myCollectionView.dataSource = self
-        myCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        //myCollectionView.translatesAutoresizingMaskIntoConstraints = false
         
         myCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: CellId)
         view.addSubview(myCollectionView)
@@ -37,15 +37,17 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
     }
     //func numberOfSections(in collectionView: UICollectionView) -> Int {
-    //    1
+    //   1
     //}
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return 30
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CellId, for: indexPath)
-        cell.backgroundColor = .lightGray
+        cell.backgroundColor = .green
+        
         return cell
+        
     }
 }
 
